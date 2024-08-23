9f228f5c9ff219c691e8ac06dc2b9d5702a6c590
Red Wang <r1ddx09@gmail.com>
2024-08-23 11:30:54 +0800

    Initial commit

---
+++ FirebaseCrashlyticsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseCrashlyticsTest
+//
+//  Created by Red Wang on 2024/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
